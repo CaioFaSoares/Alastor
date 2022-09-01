fc3d1fddaf2fc0a1c822cf91635d89de67365fb9
Caio Soares <caiofasoares@gmail.com>
2022-09-01 17:38:11 -0300

    Initial XCode configuration.

---
+++ Alastor/AlastorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlastorApp.swift
+//  Alastor
+//
+//  Created by Caio Soares on 01/09/22.
+//
+
+import SwiftUI
+
+@main
+struct AlastorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
